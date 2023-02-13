0828079afcef38062ed0cc81726a790fb6a74eb6
jrayoub <aitounaayoub05@gmail.com>
2023-02-13 19:42:10 +0100

    changes a.out parsing/start.c play.c

--- exec/builtin/cd.c
+++ exec/builtin/cd.c
@@ -1,40 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   play.c                                             :+:      :+:    :+:   */
+/*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aaitouna <aaitouna@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/10 21:07:41 by aaitouna          #+#    #+#             */
-/*   Updated: 2023/02/13 18:39:39 by aaitouna         ###   ########.fr       */
+/*   Created: 2023/02/13 18:22:45 by aaitouna          #+#    #+#             */
+/*   Updated: 2023/02/13 18:26:37 by aaitouna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./minishell.h"
+#include"built.h"
 
-void	go_to_home(void)
+void cd(char *path)
 {
-	char	cur_dir[64];
 
-	getcwd(cur_dir, 64);
-}
-
-void	change_directory(char *derictory)
-{
-	if (ft_strncmp("", derictory, 1) == 0)
-		return (go_to_home());
-	chdir(derictory);
-}
-
-int	main(void)
-{
-	chdir("/home/ayoub/1337");
-	char cur_dir[64];
-
-	getcwd(cur_dir, 64);
-	printf("cur dir <%s>", cur_dir);
-
-	chdir("");
-	getcwd(cur_dir, 64);
-	printf("cur dir <%s>", cur_dir);
 }
